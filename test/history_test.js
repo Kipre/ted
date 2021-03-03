94fc8cd2d569a225487a57ffa01138edc1cc130e
Kipre <kipr@live.ru>
2021-03-04 00:37:00 +0100

    work on history tests

--- test/history_test.js
+++ test/history_test.js
@@ -5,10 +5,24 @@ import {Cursel} from '../src/cursel.js';
 
 bro.describe('test history', _=>{
 
-    const his = new History();
-
     bro.test('store', _=>{
+        const his = new History();
         his.store([new Cursel(1,3)], ['some', 'text here'])
+        bro.expect(his.buffer).toMatchObject([{
+            cursels: [{
+                l: 1,
+                c: 3,
+                tl: null,
+                tc: null
+            }],
+            text: 'some\ntext here'
+        }])
+    }
+    )
+
+    bro.test('store async 1', async _=>{
+        const his = new History();
+        await his.store([new Cursel(1,3)], ['some', 'text here'])
         bro.expect(his.buffer).toMatchObject([{
             cursels: [{
                 l: 1,
