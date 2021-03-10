4c4acf446b88927cd6beb6c17e2fefc77559d7b2
Kipre <kipr@live.ru>
2021-03-10 08:41:21 +0100

    add service worker for installability

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import {Ted} from './ted.js';
-// navigator.permissions.query({
-//     name: 'clipboard-read'
-// });
 
-
-// if ('serviceWorker'in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('/sw.js').then(function(registration) {
-//             // Registration was successful
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }, function(err) {
-//             // registration failed :(
-//             console.log('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// }
+if ('serviceWorker'in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/src/sw.js').then((registration) => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, (err) => {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
